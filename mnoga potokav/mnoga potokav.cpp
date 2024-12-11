4c6b436b518a9fac8929cf315afcaf5206b3d035
krusberk <katya.kotel@bk.ru>
2024-12-11 15:09:35 +0300

    asdsad

---
+++ mnoga potokav/mnoga potokav.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include <thread>
+#include <windows.h>
+#include <future>
+#include <mutex>
+
+std::mutex m;
+
+void print(const char* str) {
+    for (int i = 0; i < 5; i++) {
+
+        Sleep(1000);
+        //m.lock(); Блокировка потока через Mutex
+        //Начало и автоматичекое отключение блокировки в момент выхода из функции
+        std::lock_guard<std::mutex>lock(m);
+        std::cout << "String #" << i << "\n";
+        //m.unlock(); отключение блокировки
+    }
+
+
+    //return strlen(str);
+}
+
+int main()
+{
+    //int length;
+
+    //лямбда функция
+    /*auto func = [&length](const char* str) {
+        length = print(str);
+        };
+    */
+
+    //тож лямбда
+    //std::thread thr([&length](const char* str) {length = print(str);}, "Hello, world!");//создание отдельного потока
+
+    //std::future<int> thrf = std::async(std::launch::async, print, "Hello, World!");
+
+    std::thread thr(print, "Hello, World!");
+    print("Hello, World!");
+
+    /*for (int i = 0; i < 10; i++) {
+        m.lock();//Блокировка потока через Mutex
+        Sleep(1000);
+        std::cout << "String #" << i << "\n";
+        m.unlock();
+    }*/
+
+    thr.join();//Ожидание завершения потока
+
+    //std::cout << "Length of \"Hello world\" is " << thrf.get();
+    return 0;
+}
+
---
+++ zadanie na mnogo potokav/zadanie na mnogo potokav.cpp
@@ -0,0 +1,67 @@
+﻿#include <iostream>
+#include <thread>
+#include <vector>
+#include <Windows.h>
+
+
+void menshe(std::vector <int>* mal, std::vector <int>* nums, int count) {
+    while (count != 0) {
+        for (int i = 0; i < (count - 1); i++) {
+            if ((*mal)[i] >= (*mal)[i + 1]) {
+                (*nums)[i] = (*mal)[i + 1];
+                (*mal)[i + 1] = (*mal)[i];
+            }
+            else {
+                (*nums)[i] = (*mal)[i];
+            }
+        }
+        count--;
+    }
+}
+void bolshe(std::vector <int>* bol, std::vector <int>* nums, int count) {
+    int size = count;
+    while (count != 0) {
+        for (int i = size; i < (count + (*bol).size() - 1); i++) {
+            if ((*bol)[i - size] <= (*bol)[i - size + 1]) {
+                (*nums)[i] = (*bol)[i - size + 1];
+                (*bol)[i - size + 1] = (*bol)[i - size];
+            }
+            else {
+                (*nums)[i] = (*bol)[i - size];
+            }
+        }
+        count--;
+    }
+}
+
+int main()
+{
+    setlocale(LC_ALL, "");
+    std::vector <int> nums = {1, -2, 3, 4, 0, 6, -7, 8, 9, -1};
+    std::vector <int> mal = {};
+    std::vector <int> bol = {};
+    int countMal = 0;
+    int countBol = 0;
+
+    for (int i = 0; i < nums.size(); i++) {
+        if (nums[i] <= 0) {
+            mal.push_back(nums[i]);
+            countMal++;
+        }
+        else {
+            bol.push_back(nums[i]);
+            countBol++;
+        }
+    }
+    std::thread thr1(menshe, &mal, &nums, countMal);
+    std::thread thr2(bolshe, &bol, &nums, countMal);
+    Sleep(1000);
+
+    for (int i = 0; i < nums.size(); i++) {
+
+        std::cout << nums[i] << "\n";
+    }
+
+    thr1.join();
+    thr2.join();
+}
