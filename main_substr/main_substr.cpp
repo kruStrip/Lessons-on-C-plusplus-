08446d33ee6cecb5f1f72868234086203b3e48e2
Катя <katya.kotel@bk.ru>
2025-02-05 12:55:55 +0300

    fahgdfhg

--- main_substr/main_substr.cpp
+++ main_substr/main_substr.cpp
@@ -5,7 +5,7 @@ int main()
 {
     StringWrapper str("Aalalalala");
 
-    std::string subs = str[1][10];
+    std::string subs = str[1][5];
     std::cout << subs;
 
     return 0;
--- main_substr/sub.h
+++ main_substr/sub.h
@@ -2,17 +2,30 @@
 
 class StringWrapper {
     class StringWrapperImpl {
+        int ind;
         std::string& strref;
-    public:
-        std::string operator[](int i) {
 
+    public:
+        StringWrapperImpl(std::string& str, int i) : strref(str) {
+            ind = i;
+        }
+        std::string operator[](int i) {
+            std::string substr;
+            for (int k = ind; k < i; k++) {
+                substr += strref[k];
+             
+            }
+            return substr;
         }
     };
     std::string str_;
 public:
-    StringWrapper(const char* str) : str_(str) {};
+    StringWrapper(const char* str) : str_(str) {
+    
+    };
 
     StringWrapperImpl operator[](int i) {
-
+        StringWrapperImpl s(str_, i);
+        return s;
     }
 };
