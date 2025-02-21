eb2b348f80c7b86600857999d6b0419249b1e39e
Катя <katya.kotel@bk.ru>
2025-02-21 14:57:34 +0300

    hsddvds

---
+++ Paint/paint.cpp
@@ -0,0 +1,87 @@
+#include <wapi_oop.h>
+
+class MainWindow : public Window
+{
+	std::unique_ptr<Button> b;
+
+public:
+	MainWindow(HINSTANCE hInstance) : Window(hInstance)
+	{
+		InitComponent(nullptr);
+
+		b = std::make_unique<Button>(hInstance, 10, this);
+		b->SetPosition(10, 10);
+		b->SetSize(200, 50);
+		b->SetText(L"Нажми меня");
+
+		SetText(L"Мое окно");
+		SetSize(500, 500);
+	}
+
+	LRESULT OnCommand(WPARAM wp, LPARAM lp) override
+	{
+		WORD id = LOWORD(wp), state = HIWORD(wp);
+		if (id == b->GetId() && state == BN_CLICKED)
+		{
+			MessageBoxW(hwnd, L"Вы нажали кнопку!", L"Оповещение", MB_OK);
+		}
+		return 0;
+	}
+
+	LRESULT OnPaint(HDC& hdc, PAINTSTRUCT& ps, WPARAM wp, LPARAM lp) override
+	{
+		FillRect(hdc, &ps.rcPaint, reinterpret_cast<HBRUSH>(COLOR_WINDOW + 1));
+		return 0;
+	}
+
+protected:
+	DECLARE_CLASS_NAME(MainWindow);
+};
+
+class PaintingWindow : public Window {
+	std::unique_ptr<PaintBox> pb;
+protected:
+	DECLARE_CLASS_NAME(PaintingWindow);
+public:
+	PaintingWindow(HINSTANCE hInstance) : Window(hInstance) {
+		InitComponent(nullptr);
+		pb = std::make_unique<PaintBox>(hInstance, 10, this);
+		pb->SetSize(512, 512);
+		pb->SetPosition(10, 10);
+
+		SetText(L"Паинт");
+		SetSize(550, 550);
+		//SetStyle(WS_OVERLAPPEDWINDOW | ~WS_THICKFRAME | ~WS_MAXIMIZE);
+	}
+
+	void Draw() {
+		pb->SetPen(PaintBox::Solid, 3, Color{ 252,15,192 });
+		pb->Draw<Figure::ELLIPSE>(20, 113, 80, 50);
+		pb->Draw<Figure::ELLIPSE>(140, 113, 70, 50);
+		pb->Draw<Figure::LINE>(25, 100, 100, 180);
+		pb->Draw<Figure::LINE>(140, 100, 100, 180);
+		pb->Update();
+	}
+	LRESULT OnPaint(HDC& hdc, PAINTSTRUCT& ps, WPARAM wp, LPARAM lp) override {
+		return 0;
+	}
+	/*void fill(int x1, int y1, int x2, int y2) {
+		if (pen == nullptr) throw std::logic_error("Pen is not set");
+
+		PaintParams pp{ f, x1, y1, x2, y2 };
+		paintEvents.push_back(pp);
+	}*/
+};
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR cmdline, int nCmdShow)
+{
+	REGISTER_DEFAULT(hInstance);
+	RegisterControl(PaintingWindow, hInstance);
+
+	PaintingWindow mw(hInstance);
+	mw.Show();
+	mw.Draw();
+	mw.ProcessMessages();
+
+	return 0;
+}
---
+++ saper/saper.cpp
@@ -0,0 +1,78 @@
+﻿#include <wapi_oop.h>
+#include <random>
+#include <string>
+
+class MainWindow : public Window
+{
+	std::unique_ptr<Button> b[10][10];
+	int soder[10][10];
+
+public:
+	MainWindow(HINSTANCE hInstance) : Window(hInstance)
+	{
+		int x = 10;
+		int y = 10;
+		std::random_device dev;
+		std::mt19937 rand(dev());
+		std::uniform_int_distribution<int> dist(0, 1);
+		InitComponent(nullptr);
+		for (int i = 0; i < 9; i++) {
+			for (int g = 0; g < 9; g++) {
+				b[i][g] = std::make_unique<Button>(hInstance, i*10+g, this);
+				b[i][g]->SetPosition(x, y);
+				soder[i][g] = dist(rand);
+				b[i][g]->SetSize(50, 50);
+				b[i][g]->SetText(L"*");
+				x += 50;
+			}
+			y += 50;
+			x = 10;
+		}
+
+		SetText(L"Сапер");
+		SetSize(500, 510);
+	}
+	
+
+	LRESULT OnCommand(WPARAM wp, LPARAM lp) override
+	{
+		WORD id = LOWORD(wp), state = HIWORD(wp);
+		int i = id / 10;
+		int g = id % 10;
+		int count = 0;
+		std::random_device dev;
+		std::mt19937 rand(dev());
+		std::uniform_int_distribution<int> dist(0, 8);
+		if (id == b[i][g]->GetId() && state == BN_CLICKED)
+		{	
+			if (g > 0 && soder[i][g - 1] == 1) {count++;}
+			if(g < 9 && soder[i][g + 1] == 1) {count++;}
+			if (i > 0 && soder[i+1][g] == 1) { count++; }
+			if (i < 9 && soder[i - 1][g] == 1) { count++; }
+			MessageBoxW(hwnd,(L"Вы нажали кнопку!" + std::to_wstring(count)).c_str(), L"Оповещение", MB_OK | MB_ICONINFORMATION);
+		}
+		return 0;
+	}
+
+	LRESULT OnPaint(HDC& hdc, PAINTSTRUCT& ps, WPARAM wp, LPARAM lp) override
+	{
+		FillRect(hdc, &ps.rcPaint, reinterpret_cast<HBRUSH>(COLOR_WINDOW + 1));
+		return 0;
+	}
+
+protected:
+	DECLARE_CLASS_NAME(MainWindow);
+};
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR cmdline, int nCmdShow)
+{
+	RegisterControl(MainWindow, hInstance);
+
+	MainWindow mw(hInstance);
+	mw.Show();
+
+	mw.ProcessMessages();
+
+	return 0;
+}
+
